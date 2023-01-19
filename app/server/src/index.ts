d492d0c0d226efa8b06afbb7544fc94cef8c36f7
Myos <willy14620@gmail.com>
2023-01-20 00:10:44 +0800

    :sparkles: Add graceful shutdown

--- app/server/src/index.ts
+++ app/server/src/index.ts
@@ -1,20 +1,27 @@
 import cors from 'cors';
 import express from 'express';
+import http from 'http';
+import { createTerminus } from '@godaddy/terminus';
 
 import { Workspace } from '@packages/types';
 
 const app = express();
 const port = 5000;
+const server = http.createServer(app);
 
 app.use(cors({ origin: 'http://localhost:3000' }));
 
 app.get('/workspaces', (_, response) => {
   const workspaces: Workspace[] = [
-    { name: 'api', version: '1.0.0' },
+    { name: 'client', version: '1.0.0' },
+    { name: 'server', version: '1.0.0' },
     { name: 'types', version: '1.0.0' },
-    { name: 'web', version: '1.0.0' },
   ];
   response.json({ data: workspaces });
 });
 
-app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
+createTerminus(server);
+
+server.listen(port, () =>
+  console.log(`Listening on http://localhost:${port}...`)
+);
