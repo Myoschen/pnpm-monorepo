48ae29ea52327cdf6fe940e170bfcff60b87d43a
Myos <willy14620@gmail.com>
2023-01-19 19:00:58 +0800

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# pnpm-monorepo
+
+> Monorepo with PNPM Workspace
+
+## Reference
+
+- [Building a full-stack TypeScript application with Turborepo](https://blog.logrocket.com/build-full-stack-typescript-application-turborepo/)
---
+++ packages/web/src/App.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+
+import { Workspace } from '@packages/types';
+
+function App() {
+  const [data, setData] = useState<Workspace[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('/api/workspaces');
+      const { data } = await response.json();
+      setData(data);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <h1>Monorepo with PNPM Workspace</h1>
+      <h2>Workspaces</h2>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </>
+  );
+}
+
+export default App;
